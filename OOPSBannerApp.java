76b6f0b939371663a12310f5a651e03d615bc05f
Nallana Sai Suraj <nallanasaisuraj@gmail.com>
2026-02-21 22:35:02 +0530

    Implemented UC6: Modular banner using pattern methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,28 +2,67 @@
  * OOPSBannerApp UC5 – Render OOPS as Banner using Inline Array Initialization
  *
  * @author Nallana Sai Suraj
- * @version 5.0
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
+   
+    public static String[] getOPattern() {
+        return new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        };
+    }
+
+        public static String[] getPPattern() {
+        return new String[]{
+                "*******  ",
+                "**    ** ",
+                "**     **",
+                "**    ** ",
+                "*******  ",
+                "**       ",
+                "**       ",
+                "**       ",
+                "**       "
+        };
+    }
+
+    
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " **      ",
+                "**       ",
+                " **      ",
+                "  *****  ",
+                "      ** ",
+                "       ** ",
+                "      ** ",
+                " *****  "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] lines = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-            String.join("   ", "   *** ", "     *** ", "  *******", "    ***** "),
-            String.join("   ", " **   **", "  **   **", " **    **", " **"),
-            String.join("   ", "**     **", "**     **", "**     **", "**"),
-            String.join("   ", "**     **", "**     **", "**    **", "   **"),
-            String.join("   ", "**     **", "**     **", "*******", "     ***"),
-            String.join("   ", "**     **", "**     **", "**", "             **"),
-            String.join("   ", "**     **", "**     **", "**     ", "         **"),
-            String.join("   ", " **   **", "  **   **", " **     ", "       **"),
-            String.join("   ", "   *** ", "     *** ", "  **", "        ***** ")
-
-        };
-
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    o[i] + "   " +
+                    o[i] + "   " +
+                    p[i] + "   " +
+                    s[i]
+            );
         }
     }
 }
